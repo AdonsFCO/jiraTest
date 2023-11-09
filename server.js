14669a301dfe977bded7e61c437b929cb396e7fe
AdonsFCO <52143660+AdonsFCO@users.noreply.github.com>
2023-11-09 17:01:27 -0400

    delete files and add webhookserver

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+
+// Ruta para recibir notificaciones del webhook
+app.post('/webhook-endpoint', (req, res) => {
+  const webhookData = req.body;
+  const comentario = webhookData.comment.body;
+  console.log(`Nuevo Comentario: ${comentario}`);
+  // Realiza acciones adicionales según tus necesidades
+
+  res.status(200).send('Notificación recibida');
+});
+
+const puerto = process.env.PORT || 3000;
+app.listen(puerto, () => {
+  console.log(`Servicio web en ejecución en el puerto ${puerto}`);
+});
