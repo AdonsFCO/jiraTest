b8cbdba1a7181c6a332d42ded50c3aa19f3eb491
AdonsFCO <52143660+AdonsFCO@users.noreply.github.com>
2023-11-10 10:29:21 -0400

    Add axios and add call to an specific server

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const axios = require("axios")
 
 app.use(bodyParser.json());
 
 // Ruta para recibir notificaciones del webhook
 app.post('/webhook-endpoint', (req, res) => {
-  const webhookData = req.body;
+
+  const webhookData = req.body; 
   const comentario = webhookData.comment.body;
   console.log(`Nuevo Comentario: ${comentario}`);
   // Realiza acciones adicionales según tus necesidades
+axios({
+  method: 'post',
+  url: 'https://botproto.onrender.com/comment',
+  data: {
+    ticket: webhookData.issue.key,
+    comment: webhookData.body
+  }
+});
+
 
   res.status(200).send('Notificación recibida');
+
+
 });
 
 const puerto = process.env.PORT || 3000;
