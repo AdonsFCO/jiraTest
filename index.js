5d67bd784bab94b2f27f019c3f70f65ff9b941b6
AdonsFCO <52143660+AdonsFCO@users.noreply.github.com>
2023-11-12 12:02:33 -0400

    Fix bug

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 // Ruta para recibir notificaciones del webhook
 app.post('/webhook-endpoint', (req, res) => {
   const webhookData = req.body;
-  cosnole.log(webhookData);
+  console.log(webhookData);
   // Realiza acciones adicionales según tus necesidades
 
   res.status(200).send('Notificación recibida');
